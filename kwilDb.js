55087f5cfe4f85f4108fcda34de811045e815a9a
Abdullahi-design <abdullahismohd3000@gmail.com>
2022-11-03 11:54:31 +0100

    🍁 adding reference to readme file🌿 🍁 writing to the DB 🌿

--- kwilDb.js
+++ kwilDb.js
@@ -1,3 +1,6 @@
+
+// check readme file for more clarity
+
 const KwilDB = require('kwildb');
 const fs = require('fs');
 require('dotenv').config()
@@ -19,6 +22,21 @@ const kwilDB = KwilDB.createConnector({
     privateKey: privateKey,
 }, secret)
 
-//creating a new database schema 
-kwilDB.query('CREATE TABLE newTable(secondCol varchar(20))', true)
+/**
+ * creating a new database schema 
+ * pass the name of the table in a parentheses and also pass amount or char set
+ * Sending data to kwildb requires third value to be "true" so it tells the db that it's a write file
+ */
+// kwilDB.query('CREATE TABLE newTable(secondCol varchar(20))', true)
 
+/**
+ * Sending data to kwildb requires third value to be "true" so it tells the db that it's a write file
+ * But by default it's "false" take note
+ * First pass the name of the db after passing the preparedStatement
+ * Then pass the table name in a parentheses 
+ * Pass a value
+ * then the what you are sending in an array
+ */
+
+kwilDB.preparedStatement(`INSERT INTO newTable(secondCol) VALUES ($1)`, ['My name is Abdullahi'], true)
+// kwilDB.preparedStatement(`INSERT INTO mytable(mycol) (post_id, post_text, post_owner) VALUES ($1, $2, $3)`, ['wnv47vn213re', 'Hello testing!', 'abdullahi'], true)
